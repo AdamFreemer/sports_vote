d7296d5538d7b3dafcdba85a32a7a51bd5234a94
Adam Freemer <a@freemer.com>
2018-01-04 23:10:04 -0500

    add all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
+  root 'games#index'
   resources :users
+  resources :game_types, :games, :leagues, :play_types, :plays, :reviews, :teams, :user_types, :venues
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
