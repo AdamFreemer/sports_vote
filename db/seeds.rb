40cce8da6169eb67ef17bd6166f1519f71dc07ef
Adam Freemer <a@freemer.com>
2018-01-22 00:06:29 -0500

    Update seeds, devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+
+if Rails.env.development?
+  adam_pw = CONFIG_API["adam_pw"]
+  mort_pw = CONFIG_API["mort_pw"]
+else
+  adam_pw = ENV['ADAM_PW']
+  mort_pw = ENV['MORT_PW']
+end
+
+User.create!(first_name: 'Morton', last_name: 'Gallagher', email: 'mortalicious@gmail.com', password: mort_pw, password_confirmation: mort_pw)
+User.create!(first_name: 'Adam', last_name: 'Freemer', email: 'a@freemer.com', password: adam_pw, password_confirmation: adam_pw)
+
 Team.create(name: 'Flyers')
 Team.create(name: 'Oilers')
 Team.create(name: 'Devils')
@@ -17,6 +25,4 @@ Venue.create(name: 'Hatfield Ice Arena', address: '350 County Line Rd, Colmar, P
 Venue.create(name: 'Vidas Athletic Complex', address: '4300 Powelton Ave, Philadelphia, PA 19104')
 League.create(name: 'Minor 1', age_upper: 18, age_lower: 6)
 League.create(name: 'Minor 2', age_upper: 6, age_lower: 4)
-
-
-
+puts "== Seeds created ==================="
