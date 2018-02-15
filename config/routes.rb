b67a29222d693b0474d3eee763f8ba41a62517f4
Adam Freemer <a@freemer.com>
2018-02-15 15:08:48 -0500

    Updated routes and config for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   resources :game_types, :leagues, :play_types, :plays, :reviews, :teams, :user_types, :venues
 
   resources :games do
-    resources :plays do
-      resources :reviews
-    end
+    resources :plays
+  end
+
+  resources :plays do
+    resources :reviews
   end
 end
